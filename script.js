f213ba235d0fd27b8197782b2c6af7c8c4e4e43d
Hridoy Biswas Dipu <94591361+hridoy2302@users.noreply.github.com>
2022-09-10 15:45:32 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+const bodyEl = document.querySelector("body");
+
+
+bodyEl.addEventListener("mousemove", (heart)=>{
+    const xPos = heart.offsetX;
+    const yPos = heart.offsetY;
+
+    const spanEl = document.createElement("span");
+
+    spanEl.style.left = xPos + "px";
+    spanEl.style.top = yPos + "px";
+
+    const size = Math.random() * 100;
+    spanEl.style.width = size + "px"
+    spanEl.style.height = size + "px"
+
+    bodyEl.appendChild(spanEl);
+
+    setTimeout(()=>{
+        spanEl.remove();
+    },3000);
+
+
+})
